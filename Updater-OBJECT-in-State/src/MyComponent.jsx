c9ec03083fd82c30e5b5b2308f2c8130a78a2a65
tharshanth705 <dharshanthg.13@gmail.com>
2026-08-17 23:31:54 +0530

    Updated CSS in Folder: Updater Obj in State

--- Updater-OBJECT-in-State/src/MyComponent.jsx
+++ Updater-OBJECT-in-State/src/MyComponent.jsx
@@ -1,30 +1,58 @@
-
 import { useState } from "react";
 
 function MyComponent() {
 
-    const [car, setCar] = useState({ year: 2024, make: "Ford", model: "mustang" });
+    const [car, setCar] = useState({
+        year: 2024,
+        make: "Ford",
+        model: "mustang"
+    });
 
     function handleYearChange(event) {
         setCar(c => ({ ...c, year: event.target.value }));
-
     }
+
     function handleMakeChange(event) {
         setCar(c => ({ ...c, make: event.target.value }));
     }
+
     function handleModelChange(event) {
         setCar(c => ({ ...c, model: event.target.value }));
     }
 
     return (
-        <div>
-            <p>Your Favourite Car is : {car.year} {car.make} {car.model}</p>
+        <div className="car-container">
 
-            <input type="number" value={car.year} onChange={handleYearChange} /><br />
-            <input type="text" value={car.make} onChange={handleMakeChange} /><br />
-            <input type="text" value={car.model} onChange={handleModelChange} /><br />
+            <p className="car-title">
+                Your Favourite Car is:{" "}
+                <span>
+                    {car.year} {car.make} {car.model}
+                </span>
+            </p>
+
+            <input
+                className="car-input"
+                type="number"
+                value={car.year}
+                onChange={handleYearChange}
+            />
+
+            <input
+                className="car-input"
+                type="text"
+                value={car.make}
+                onChange={handleMakeChange}
+            />
+
+            <input
+                className="car-input"
+                type="text"
+                value={car.model}
+                onChange={handleModelChange}
+            />
+
         </div>
-
     );
 }
-export default MyComponent
+
+export default MyComponent;
